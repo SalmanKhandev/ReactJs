96ce17276fcfe2810d05c3ae7983d7cde18d3654
Salman Khan <salmankhanuet99@gmail.com>
2021-12-24 00:42:48 +0500

    integrate base64 converion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import FileBase64 from "react-file-base64";
+import axios from "axios";
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    loadImages();
+  }, []);
+
+  const loadImages = async () => {
+    const imagesArray = [];
+
+    const images = await axios.get(
+      "http://52.5.72.137:3000/adminimages/61937e3f851ac8d517f83818"
+    );
+
+    for (let i = 0; i < images.data.data.length; i++) {
+      const file = images.data.data[i].image;
+      const data = await fetch(file);
+      const blob = await data.blob();
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onload = (readerEvt) => {
+        const base64data = readerEvt.target.result;
+        var arr = base64data.split(",");
+        var base64 = arr[arr.length - 1];
+        imagesArray.push(base64);
+      };
+    }
+    setFiles(imagesArray);
+  };
+
   return (
     <div>
       <h1>Client is perfectly Setup & Running</h1>
+      {files.map((setting) => (
+        <img
+          src={`data:image/png;base64,${setting}`}
+          style={{ width: "150px", height: "100px" }}
+        />
+      ))}
     </div>
   );
 }
